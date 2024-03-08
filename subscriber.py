2f19da9838a4f585329a1b9ed7f3fb61aff69793
J1ngy1 <jingyi.xu@wustl.edu>
2024-03-08 20:03:56 +0000

    magnetometer publisher and subscriber that listens to it and the lidar

--- subscriber.py
+++ subscriber.py
@@ -1,19 +1,29 @@
 #!/usr/bin/env python
 import rospy
-from sensor_msgs.msg import Imu
 from  beginner_tutorials.msg import Magnetometer
 
-def imu_callback(data):
-    rospy.loginfo("IMU Data: Accel=(%f, %f, %f), Gyro=(%f, %f, %f)", data.linear_acceleration.x, data.linear_acceleration.y, data.linear_acceleration.z, data.angular_velocity.x, data.angular_velocity.y, data.angular_velocity.z)
+from sensor_msgs.msg import LaserScan
+
+def callback(data):
+    if data.ranges:
+        rospy.loginfo('Received LiDAR data at the back: %f',data.ranges[100])
+    else:
+        rospy.loginfo('Out of scan range')
+
+def listener():
+    rospy.init_node('lidar_listener', anonymous=True)
+    rospy.Subscriber('/scan', LaserScan, callback)
+    rospy.Subscriber('magnetometer_data', Magnetometer, mag_callback)
+    rospy.spin()
 
 def mag_callback(data):
     rospy.loginfo("Magnetometer Data: Mag=(%f, %f, %f)", data.mag_x, data.mag_y, data.mag_z)
 
 def listen_to_sensors():
     rospy.init_node('sensor_subscriber', anonymous=True)
-    rospy.Subscriber('imu_data', Imu, imu_callback)
     rospy.Subscriber('magnetometer_data', Magnetometer, mag_callback)
     rospy.spin()
 
 if __name__ == '__main__':
-    listen_to_sensors()
+    listener()
+
